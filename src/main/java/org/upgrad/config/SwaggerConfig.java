923640d1246967d71af81f4c70f635745c2371c0
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-09 14:52:00 +0100

    Update from locally successful build

--- src/main/java/org/upgrad/config/SwaggerConfig.java
+++ src/main/java/org/upgrad/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket productsApi(){
+    public Docket productsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.upgrad.controllers"))
