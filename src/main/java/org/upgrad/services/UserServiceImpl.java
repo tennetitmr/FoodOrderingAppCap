ac88bc4601f0e76273ee52f1c2d37c4a4cb9c612
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-09 16:34:42 +0100

    after green build in local

--- src/main/java/org/upgrad/services/UserServiceImpl.java
+++ src/main/java/org/upgrad/services/UserServiceImpl.java
@@ -2,16 +2,13 @@ package org.upgrad.services;
 
 import org.springframework.stereotype.Service;
 import org.upgrad.models.User;
-import org.upgrad.models.UserAuthToken;
-import org.upgrad.repositories.UserAuthTokenRepository;
 import org.upgrad.repositories.UserRepository;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -29,4 +26,35 @@ public class UserServiceImpl implements UserService{
         return userRepository.findUser(contactNumber);
     }
 
+    @Override
+    public Boolean newUser(String firstName, String lastName, String email, String contactNumber, String password) {
+        Boolean success = false;
+        userRepository.newUser(firstName, lastName, email, contactNumber, password);
+        if (userRepository.findUser(contactNumber) != null) {
+            success = true;
+        }
+        return success;
+    }
+
+    @Override
+    public User updateUser(String firstName, String lastName, int id) {
+        int updateUser = userRepository.updateUser(firstName, lastName, id);
+        User user = userRepository.getUserById(id);
+        return user;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return userRepository.getUserById(id);
+    }
+
+    @Override
+    public String findUserPwdById(int id) {
+        return userRepository.findUserPwdById(id);
+    }
+
+    @Override
+    public void updatePwd(String newPassword, int id) {
+        userRepository.updatePwd(newPassword, id);
+    }
 }
