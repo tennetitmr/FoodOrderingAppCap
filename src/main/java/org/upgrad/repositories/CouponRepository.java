d49df04656353d18dd717bdbc0c6ddbf2ffdde2c
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-09 15:41:56 +0100

    Update CouponRepository.java

--- src/main/java/org/upgrad/repositories/CouponRepository.java
+++ src/main/java/org/upgrad/repositories/CouponRepository.java
@@ -1,4 +1,13 @@
 package org.upgrad.repositories;
 
-public interface CouponRepository{
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upgrad.models.Coupon;
+
+@Repository
+public interface CouponRepository extends CrudRepository<Coupon, Integer> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM COUPON  WHERE coupon_name = ?1")
+    Coupon findCouponByName(String couponName);
 }
