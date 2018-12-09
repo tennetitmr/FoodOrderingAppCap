988f3b5acc87ceeccb9489df362a5ab4c0d4bbdf
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-09 16:33:35 +0100

    Fixed issues fromt unit test

--- src/main/java/org/upgrad/services/UserService.java
+++ src/main/java/org/upgrad/services/UserService.java
@@ -1,17 +1,23 @@
 package org.upgrad.services;
 
 import org.upgrad.models.User;
-import org.upgrad.models.UserAuthToken;
 
-import java.util.Optional;
-
-/*
- * This UserService interface gives the list of all the service that exist in the user service implementation class.
- * Controller class will be calling the service methods by this interface.
+/* Author: Mohan
+ * This UserService interface has the list of all services in the User Service Implementation Class.
  */
 public interface UserService {
 
     String findUserPassword(String contactNumber);
 
     User findUser(String contactNumber);
+
+    Boolean newUser(String firstName, String lastName, String email, String contactNumber, String password);
+
+    User updateUser(String firstName, String lastName, int id);
+
+    User getUserById(int id);
+
+    String findUserPwdById(int id);
+
+    void updatePwd(String newPassword, int id);
 }
