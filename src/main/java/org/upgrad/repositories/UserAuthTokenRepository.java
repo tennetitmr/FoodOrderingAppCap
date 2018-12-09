243408b5c2bb45788f9ef695f6c1ba2de08f2dee
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-09 15:49:07 +0100

    Update UserAuthTokenRepository.java

--- src/main/java/org/upgrad/repositories/UserAuthTokenRepository.java
+++ src/main/java/org/upgrad/repositories/UserAuthTokenRepository.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.upgrad.models.UserAuthToken;
 
-// This repository interface is responsible for the interaction between the user auth token service with the user_auth_token database
+//  author: mohan 
 
 @Repository
 public interface UserAuthTokenRepository extends CrudRepository<UserAuthToken, Integer> {
 
-    @Query(nativeQuery = true,value = "SELECT * FROM USER_AUTH_TOKEN WHERE access_token=?1")
+    @Query(nativeQuery = true, value = "SELECT * FROM USER_AUTH_TOKEN WHERE access_token=?1")
     UserAuthToken isUserLoggedIn(String accessToken);
 
     @Transactional
     @Modifying
-    @Query(nativeQuery = true,value="UPDATE USER_AUTH_TOKEN SET logout_at=NOW() WHERE access_token=?1")
-    void removeAuthToken( String accessToken);
+    @Query(nativeQuery = true, value = "UPDATE USER_AUTH_TOKEN SET logout_at=NOW() WHERE access_token=?1")
+    void removeAuthToken(String accessToken);
+
+    @Query(nativeQuery = true, value = "select user_id from user_auth_token where access_token=?1")
+    int getUserId(String accessToken);
 }
